0ea254a6eea947e9ef788170f6b940a00b5144ee
CaitHawk <78328496+CaitHawk@users.noreply.github.com>
2023-06-27 07:56:13 -0700

    adds areas of practice

--- src/Components/Organisms/AreaOfPractice/index.js
+++ src/Components/Organisms/AreaOfPractice/index.js
@@ -1,17 +1,47 @@
 import './styles.css';
-import { Button } from '@mui/material';
+import Button from '../../Atoms/Button';
 
 const AreaOfPractice = () => {
-    return (
-        <section className='areasOfPracticeSection'>
-            <p className='practiceAreasSubtitle'>Practice Areas</p>
-            <h2 className='practiceAreasTitle'>Areas of Practice</h2>
-            <p className='practiceAreasP1'></p>
-            <p className='practiceAreasP2'></p>
-            <p className='practiceAreasP3'></p>
-            <Button color='orange' copy='Free Consultation'/>
-        </section>
-    )
+	return (
+		<section className='areasOfPracticeSection'>
+			<div className='practiceAreasTextDiv'>
+				<p className='practiceAreasSubtitle'>Practice Areas</p>
+				<h2 className='practiceAreasTitle'>Areas of Practice</h2>
+				<p className='practiceAreasP1'>
+					Our law firm offers a range of legal services to help you navigate
+					your legal needs. We specialize in various practice areas, including
+					family law, bankruptcy law, and criminal law.
+				</p>
+
+				<p className='practiceAreasP2'>
+					Our family law practice encompasses issues such as divorce, child
+					custody, and child support to ensure that your family's best interests
+					are protected. Our bankruptcy law practice helps you navigate the
+					complexities of bankruptcy filings and proceedings. Our criminal law
+					practice assists with DUI defense, drug crimes, and other criminal
+					charges, so that you can focus on your defense while we take care of
+					the legal details.
+				</p>
+
+				<p className='practiceAreasP3'>
+					Call us at{' '}
+					<a
+						href='tel:+18086206392'
+						className='phoneLink'
+					>
+						{'(808) 620-6392'}
+					</a>{' '}
+					today to schedule a consultation and learn how we can help you with
+					your legal needs.
+				</p>
+				<Button
+					color='orange'
+					copy='Free Consultation'
+					className='practiceAreasButton'
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default AreaOfPractice;
--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -3,17 +3,21 @@ import InfoCards from '../Components/Organisms/InfoCards';
 import Testimonials from '../Components/Organisms/Testimonials';
 import ContactBar from '../Components/Molecules/ContactBar';
 import Footer from '../Components/Organisms/Footer';
+import AreaOfPractice from '../Components/Organisms/AreaOfPractice';
 
 export default function HomeRouter() {
-    return (
-        <main>
-           <PageHead 
-            title='A trusted advisor for your business and your o’hana'
-            copy='Legal matters can be overwhelming and stressful. That is why we offer personalized attention and support to ensure that you feel comfortable and confident throughout the legal process.' button={true} />
-            <InfoCards />
-            <Testimonials />
-            <ContactBar />
-            <Footer />
-        </main>
-    )
-};
+	return (
+		<main>
+			<PageHead
+				title='A trusted advisor for your business and your o’hana'
+				copy='Legal matters can be overwhelming and stressful. That is why we offer personalized attention and support to ensure that you feel comfortable and confident throughout the legal process.'
+				button={true}
+			/>
+			<InfoCards />
+			{/* <AreaOfPractice /> */}
+			<Testimonials />
+			<ContactBar />
+			<Footer />
+		</main>
+	);
+}
