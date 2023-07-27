b5b3b6dd2af0d16c160fb15195ebdb81c9fb447c
CaitHawk <78328496+CaitHawk@users.noreply.github.com>
2023-07-27 11:42:11 -0700

    adds redirects file

--- src/Components/Molecules/NavLinks/index.js
+++ src/Components/Molecules/NavLinks/index.js
@@ -1,16 +1,25 @@
 import './styles.css';
 
 const NavLinks = () => {
-    const linksList = ['about', 'services', 'contact', 'resources'];
-    return (
-        <ol className='linksOl'>
-            { linksList.map( link => (
-                <li key={`${link}link`}>
-                    <a href={`/${link}`} className='link'>{link}</a>
-                </li>
-            ))}
-        </ol>
-    )
+	const linksList = ['about', 'services', 'contact', 'resources'];
+	return (
+		<ol className='linksOl'>
+			{linksList.map(link => (
+				<li key={`${link}link`}>
+					<a
+						href={`${
+							link !== 'resources'
+								? `/${link}`
+								: 'www.courts.state.hi.us/self-help/help'
+						}`}
+						className='link'
+					>
+						{link}
+					</a>
+				</li>
+			))}
+		</ol>
+	);
 };
 
 export default NavLinks;
