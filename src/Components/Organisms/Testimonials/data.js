1a14d525ae38a51fd102ba240ff23760d705531e
CaitHawk <78328496+CaitHawk@users.noreply.github.com>
2023-09-07 16:16:17 -0700

    open graph image link fix

--- src/Components/Organisms/Testimonials/data.js
+++ src/Components/Organisms/Testimonials/data.js
@@ -1,19 +1,19 @@
 const quotes = [
-    {
-        header: 'Top Dog Lawyer',
-        copy: 'If you\'re in the dog house and you need strong legal support, HIRE Mrs. Heckler! She always has multiple strategies to fight for you in court. She executes cross examination of the opposing party like a scripted TV show! She did great thing for me and I know she can do great things for you! Very decent retainer, it didn\'t break my wallet.',
-    },
-    {
-        header: 'GREAT Attorney',
-        copy: 'Andrea helped me on a couple of matters while I lived on Oahu. Now that I\'m on the mainland I still call for her advice. Would HIGHLY recommend her to any one on Oahu that needs legal help!!!',
-    }
+	{
+		header: 'Top Dog Lawyer',
+		copy: "If you're in the dog house and you need strong legal support, HIRE Mrs. Heckler! She always has multiple strategies to fight for you in court. She executes cross examination of the opposing party like a scripted TV show! She did great thing for me and I know she can do great things for you! Very decent retainer, it didn't break my wallet.",
+	},
+	{
+		header: 'GREAT Attorney',
+		copy: "Andrea helped me on a couple of matters while I lived on Oahu. Now that I'm on the mainland I still call for her advice. Would HIGHLY recommend her to any one on Oahu that needs legal help!!!",
+	},
 ];
 
 const titleData = {
-    subtitle: 'Testimonials',
-    title: 'What Passed Clients Have Said',
-    color: '#333',
-    background: '#fff'
-}
+	subtitle: 'Testimonials',
+	title: 'What Past Clients Have Said',
+	color: '#333',
+	background: '#fff',
+};
 
 export { quotes, titleData };
